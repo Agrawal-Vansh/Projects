6b4455079087490dd72fc1fed00b5054ed54ffb5
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2024-07-18 19:33:56 +0530

    Search bar created

--- Weather/tailwind.config.js
+++ Weather/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"],
+  content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
+        'custom-gradient1': 'linear-gradient(39deg, #85FFBD 31%, #FFFB7D 85%)',
+
+      },
+    },
   },
   plugins: [],
 }
